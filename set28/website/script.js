f60a6cee52c25fadf6cffca0be053fe1f4a4b59d
This-is-Luke <lluminous.210@gmail.com>
2023-04-27 19:52:27 +0200

    Task 28, website

---
+++ set28/website/script.js
@@ -0,0 +1,7 @@
+
+  document.addEventListener('DOMContentLoaded', function () {
+    setTimeout(function () {
+      document.querySelector('img.fade-in').classList.add('start-animation');
+    }, 1000); // Adjust the delay time (in milliseconds) as needed
+  });
+
